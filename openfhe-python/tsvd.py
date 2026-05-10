1f80ed99dcc0d3f30b83538e262126e9f8eae5a8
Li Xuan Tan <lixuan02@gmail.com>
2026-05-10 16:10:33 -0400

    integrate subspace iteration into randomized reduction TSVD

--- openfhe-python/tsvd.py
+++ openfhe-python/tsvd.py
@@ -1,15 +1,16 @@
 from openfhe import *
-from matrix import matrix_multiply, transpose
+from matrix import matrix_multiply, transpose, repack
+from subspace_iteration import subspace_iteration_fhe
 import random
 
-def truncated_svd(A:Ciphertext, n:int, cc:CryptoContext, pk:PublicKey, alpha:float=0.01, iters:int=4):
+def truncated_svd(A:Ciphertext, n:int, k:int, cc:CryptoContext, pk:PublicKey, alpha:float=0.01, iters:int=4):
     """
     Halko randomized algorithm for TSVD
-    A is an n by n matrix to find the TSVD of
+    A is an n by n matrix to find the first k SVDs of
     """
 
-    # Generate random Gaussian matrix and encrypt
-    rand_ptx = [random.gauss(0, 1) for i in range(n ** 2)]
+    # Generate random Gaussian n by k matrix (zero-padded into n by n) and encrypt
+    rand_ptx = [(random.gauss(0, 1) if (i%n<k) else 0.0) for i in range(n ** 2)]
     rand_ctx = cc.Encrypt(pk, cc.MakeCKKSPackedPlaintext(rand_ptx))
 
     # Compute random sample matrix from A and its transpose
@@ -28,26 +29,22 @@ def truncated_svd(A:Ciphertext, n:int, cc:CryptoContext, pk:PublicKey, alpha:flo
 
     arr_3I = [(3 if (i%n) == (i//n) else 0) for i in range(n ** 2)]
     ptx_3I = cc.MakeCKKSPackedPlaintext(arr_3I)
-
     ctx_3I = cc.Encrypt(pk, ptx_3I)
 
     for i in range(iters):
 
+        # optimizing for speed
         # X_sq = matrix_multiply(X, X, n, cc, pk)
         # M_X_sq = matrix_multiply(M, X_sq, n, cc, pk)
         # diff = cc.EvalSub(ptx_3I, M_X_sq)
-        # X = matrix_multiply(X, diff, n, cc, pk)
+        # X = matrix_multiply(X, diff, n, cc, pk, c=0.5)
         
         # optimizing for mult depth
         X_sq = matrix_multiply(X, X, n, cc, pk)
         X_M = matrix_multiply(X, M, n, cc, pk)
-
         t1 = matrix_multiply(X, ctx_3I, n, cc, pk, c=0.5)
         t2 = matrix_multiply(X_M, X_sq, n, cc, pk, c=0.5)
-
         X = cc.EvalSub(t1, t2)
-
-        # X = cc.EvalMult(X, 0.5)
 
         print("Newton-Schulz iter", i)
 
@@ -55,13 +52,17 @@ def truncated_svd(A:Ciphertext, n:int, cc:CryptoContext, pk:PublicKey, alpha:flo
     Q = matrix_multiply(Y, X, n, cc, pk)
     Q_T = transpose(Q, n, cc, pk)
 
-    # project into subspace and compute standard SVD of projection B
+    # project into subspace B
     B = matrix_multiply(Q_T, A, n, cc, pk)
+
+    # repack into row-major k by k matrix instead of n by n
+    B = repack(B, n, k, cc, pk)
 
     # bootstrap to get more depth
     print("begin bootstrapping, level", B.GetLevel())
     B = cc.EvalBootstrap(B)
     print("bootstrapped to level", B.GetLevel())
+    B_T = transpose(B, n, cc, pk)
 
-    # TODO update
-    return B
+    # compute standard SVD of projection B
+    return subspace_iteration_fhe(cc, pk, B, B_T, k, k)
